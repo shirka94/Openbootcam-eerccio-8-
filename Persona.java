a29ad9d72e11ead308436410ec372fddff656b85
shirka94 <106527168+shirka94@users.noreply.github.com>
2022-06-27 01:28:35 +0200

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,38 @@
+public class Persona {
+    private int edad;
+    private String nombre;
+    private int telefono;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(int edad, String nombre, int telefono) {
+        this.edad = edad;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public Persona() {
+    }
+}
